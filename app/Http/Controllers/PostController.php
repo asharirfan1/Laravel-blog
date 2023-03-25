1697e7c39829f1fa833e47baf26fc831365cfe0c
asharirfan1 <asharirfan26@gmail.com>
2023-03-25 15:35:02 +0500

    blog post

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\post;
+use Illuminate\Http\Request;
+use Random\Engine\Mt19937;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index( Request $request)
+    {
+        $search = $request['search'] ?? '';
+        if($search != '')
+        {
+            //where
+            $posts = Post::where('title' , 'like' , "%$search%")->get();
+        }
+        else
+        {
+            $posts = Post::where('user_id', auth()->user()->id)->get();
+        }
+
+
+        return view('Post.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $post = new Post();
+        $post->user_id = auth()->user()->id;
+
+        return view('Post.create', compact('post'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Post::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'user_id' => auth()->user()->id,
+
+        ]);
+
+
+        return redirect()->route("post.index");
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(post $post)
+    {
+        return view('Post.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, post $post)
+    {
+
+        $post->update([
+            'title' => $request->title,
+            'content' => $request->content
+
+        ]);
+        return redirect()->route("post.index")->with('success', 'post updated successfuly');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(post $post)
+    {
+        $post->delete();
+        return redirect()->route('post.index');
+    }
+}
---
+++ resources/views/Post/index.blade.php
@@ -0,0 +1,89 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Posts list') }}
+        </h2>
+    </x-slot>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <a href="{{route('post.create')}}"> Add new post</a>
+
+
+                    <div class="container">
+
+                        <div class="row height d-flex justify-content-center align-items-center">
+
+                            <div class="col-md-6">
+                                <form method="get" action="{{ route('post.index') }}">
+                                    <div class="form">
+                                        <i class="fa fa-search"></i>
+                                        <input type="search" name="search" value="{{request('search')}}"
+                                               class="form-control form-input" placeholder="Search posts...">
+                                        <span>
+                                            <button type="submit" class="btn btn-outline-primary mt-2">search</button>
+                                        </span>
+                                    </div>
+                                </form>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+                    <br> <br>
+
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th class="pl-6">Content</th>
+                            <th>Publication date</th>
+
+
+                            <th></th>
+                        </tr>
+
+                        </thead>
+                        <tbody>
+                        @foreach($posts as $post)
+                            <tr>
+                                <td>{{$post->title}}</td>
+                                <td class="pl-4">{{$post->content}}</td>
+                                <td class="pl-4 font-semibold">{{$post->created_at}}</td>
+
+                                <td><a href="{{ route('post.edit', $post) }}"
+                                       class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full ml-4">
+                                        Edit</a></td>
+
+
+                                <form method="POST" action="{{route('post.destroy',$post)}}">
+                                    @csrf
+                                    @method('Delete')
+
+                                    <td>
+                                        <button type="submit" onclick="return confirm('Are you sure ?')"
+                                                class="btn btn-danger" style="border-radius:25px; ">
+
+                                            Delete
+                                        </button>
+                                    </td>
+
+                                </form>
+
+
+                            </tr>
+                        @endforeach
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
